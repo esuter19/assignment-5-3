cea44679f5f88c817b8ea324bbee619067f57909
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-24 21:58:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
-        });
-
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
-        });
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+	  "esri/widgets/Home",
+      "esri/widgets/Legend",
+      "esri/widgets/LayerList",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home, Legend, LayerList) {
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
       });
+      
+      var camera = new Camera({
+        position: [
+           -71.060217,// lon
+          42.382655, // lat
+          2500// elevation in meters
+        ],
+        tilt:45,
+        heading: 180
+      })
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        camera: camera
+    });
+	
+	var homeBtn = new Home({
+        view: view
+      });
+      
+      view.when(function() {
+        var featureLayer = scene.layers.getItemAt(1);
+
+        var legend = new Legend({
+          view: view,
+          layerInfos: [{
+            layer: featureLayer,
+            title: "Major project buildings"
+          }]
+        });
+      
+   view.ui.add(legend, "bottom-right");
+        
+        var layerList = new LayerList({
+  view: view
+});
+        view.ui.add(layerList, "bottom-right");
+   });
+    view.ui.add(homeBtn, "top-left");
+
+
+    });
